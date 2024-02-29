93f74a8b64631e86a1fd9b5fd5bee239c4dbf587
Chris Browet <cbro@semperpax.com>
2024-02-29 14:08:45 +0100

    Revert "Fix nice flor-s"
    This reverts commit 3dc5f5e4c88910c1e60396a70221658cc66a081a.

--- include/nice_flor_s_table_decode.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef INCLUDE_NICE_FLOR_S_TABLE_DECODE_H_
-#define INCLUDE_NICE_FLOR_S_TABLE_DECODE_H_
-
-extern uint16_t nice_flor_s_table_decode[65536];
-
-#endif
-
--- include/nice_flor_s_table_ki.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef INCLUDE_NICE_FLOR_S_TABLE_KI_H_
-#define INCLUDE_NICE_FLOR_S_TABLE_KI_H_
-
-extern uint8_t nice_flor_s_table_ki[256];
-
-#endif
